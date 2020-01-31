3d97a45ef2715be38a92bd9516d89d15b4533627
PhongBV <buivan.phong94@gmail.com>
2020-01-31 07:46:46 +0700

    Update summary

--- FindForUpdate/DAO/Extensions/EntityExtension.cs
+++ FindForUpdate/DAO/Extensions/EntityExtension.cs
@@ -18,7 +18,17 @@ namespace FindForUpdate.DAO.Extensions
     public static class EntityExtension
     {
         static ConcurrentDictionary<string, TableMapping> tablesMapping = new ConcurrentDictionary<string, TableMapping>();
-       
+        /// <summary>
+        /// Get record from DbSet and lock it in Db
+        /// <para>
+        /// Exception when current update seq pass from parameter differs current update seq from database
+        /// </para>
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="dbSet">DbSet</param>
+        /// <param name="updSeq">Update sequence from model</param>
+        /// <param name="keyValues">All key values in current DbSet(Current table)</param>
+        /// <returns>Record matches <paramref name="keyValues"/>. If it does not exist return null</returns>
         public static T FindForUpdate<T>(this DbSet<T> dbSet, decimal updSeq, params object[] keyValues) where T : IAuditEntity
         {
             using (var dbContext = new DataContext())
@@ -41,17 +51,34 @@ namespace FindForUpdate.DAO.Extensions
             }
         }
 
-        private static void DoLock<T>(this DbSet<T> dbSet, params object[] keyValues) where T : class
-        {
-            DoLock(dbSet, null, keyValues);
-        }
+        /// <summary>
+        /// Lock current object by primary keys
+        /// </summary>
+        /// <typeparam name="T">Current object type</typeparam>
+        /// <param name="dbContext">Current dbContext</param>
+        /// <param name="keyValues">All values for table keys</param>
+        public static void DoLock<T>(this DbContext dbContext, params object[] keyValues) where T : class => DoLock(dbContext.Set<T>(), dbContext, keyValues);
+        /// <summary>
+        /// Lock current object by primary keys
+        /// </summary>
+        /// <typeparam name="T">Current object type</typeparam>
+        /// <param name="keyValues">All values for table keys</param>
+        private static void DoLock<T>(this DbSet<T> dbSet, params object[] keyValues) where T : class => DoLock(dbSet, null, keyValues);
+        /// <summary>
+        /// Lock current object by primary keys
+        /// </summary>
+        /// <typeparam name="T">Current object type</typeparam>
+        /// <param name="dbSet">Current object type's DbSet </param>
+        /// <param name="dbContext">Current dbContext</param>
+        /// <param name="keyValues">All values for table keys</param>
         private static void DoLock<T>(this DbSet<T> dbSet, DbContext dbContext = null, params object[] keyValues) where T : class
         {
+            // Initialize dbContext from parameter or from dbSet
             dbContext = dbContext ?? dbSet.GetContext();
-
+            // Get all key name in current table
             var _keyValuePairs = dbSet.GetEntityKeys().Zip(keyValues, (name, value) => new KeyValuePair<string, object>(name, value));
             var queryBuilder = new StringBuilder();
-            queryBuilder.AppendFormat("SELECT 1 FROM {0} X WHERE ", GetTableMapping(typeof(T),dbContext).TableName);
+            queryBuilder.AppendFormat("SELECT 1 FROM {0} X WHERE ", GetTableMapping(typeof(T), dbContext).TableName);
 
             var entityKeyValues = _keyValuePairs.ToArray();
             var parameters = new OracleParameter[entityKeyValues.Length];
@@ -70,7 +97,12 @@ namespace FindForUpdate.DAO.Extensions
             queryBuilder.Append(" FOR UPDATE");
             dbContext.Database.ExecuteSqlCommand(queryBuilder.ToString(), parameters);
         }
-
+        /// <summary>
+        /// Get DbContext from DbSet
+        /// </summary>
+        /// <typeparam name="TEntity"></typeparam>
+        /// <param name="dbSet"></param>
+        /// <returns></returns>
         private static DbContext GetContext<TEntity>(this DbSet<TEntity> dbSet)
             where TEntity : class
         {
@@ -88,25 +120,32 @@ namespace FindForUpdate.DAO.Extensions
                 .GetProperty("Owner", BindingFlags.Instance | BindingFlags.Public)
                 .GetValue(internalContext, null);
         }
+        /// <summary>
+        /// Get keys of entity
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="dbSet"></param>
+        /// <returns></returns>
+        private static IEnumerable<string> GetEntityKeys<T>(this DbSet<T> dbSet) where T : class => GetEntityKeys<T>(dbSet.GetContext());
 
-        private static IEnumerable<string> GetEntityKeys<T>(this DbSet<T> dbSet) where T : class
-        {
-            return GetEntityKeys<T>(dbSet.GetContext());
-        }
-
-        private static IEnumerable<string> GetEntityKeys<T>(this DbContext dbContext) where T : class
+        private static List<string> GetEntityKeys<T>(this DbContext dbContext) where T : class
         {
             ObjectContext objectContext = ((IObjectContextAdapter)dbContext).ObjectContext;
             var metada = objectContext.MetadataWorkspace.GetItemCollection(DataSpace.SSpace).GetItems<EntityContainer>().Single()
                      .BaseEntitySets
                      .OfType<EntitySet>().First();
             ObjectSet<T> set = objectContext.CreateObjectSet<T>();
-            IEnumerable<string> keyNames = set.EntitySet.ElementType
+            List<string> keyNames = set.EntitySet.ElementType
                                                         .KeyMembers
-                                                        .Select(k => k.Name);
+                                                        .Select(k => k.Name).ToList();
             return keyNames;
         }
-
+        /// <summary>
+        /// Get table and entity mappings
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="dbContext"></param>
+        /// <returns></returns>
         private static TableMapping GetTableMapping(Type type, DbContext dbContext)
         {
             if (tablesMapping.ContainsKey(type.Name)) return tablesMapping[type.Name];
